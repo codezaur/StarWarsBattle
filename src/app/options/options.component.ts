5125ce10b3b5d86c479edc718dd81c5a46757853
DominikLipinski <maboonga@gmail.com>
2020-01-04 20:51:43 +0100

    displaying winning conditions in options component

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -13,11 +13,24 @@ export class OptionsComponent implements OnInit {
                          {name: 'people', photo: 'vader'}];
   selectedBattleType: string = 'species';
 
+  showWinningOptions: boolean = null;
+  winningOptions: any[] = [];
+
   constructor(private optionsService: OptionsService) { }
 
   setBattleType(type: string): void {
+    console.log(type);
     this.selectedBattleType = type;
     this.optionsService.setBattleType(type);
+  }
+
+  toggleWinningOptions() {
+    this.showWinningOptions = this.showWinningOptions === (false || null) ? true : false;
+
+    this.winningOptions = this.optionsService.getWiningConditions(this.selectedBattleType);
+
+    if (this.showWinningOptions === false) {
+      setTimeout(() => { this.showWinningOptions = null; }, 400); }
   }
 
   ngOnInit() {
